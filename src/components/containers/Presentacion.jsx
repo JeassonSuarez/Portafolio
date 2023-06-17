7f079d9f2283ffb77b1637d403c1d4cd6bcd8df7
jeassons <jeassonsuarez@outlook.es>
2023-06-16 22:41:48 -0500

    agregando algunos estilos y foto de perfil

--- src/components/containers/Presentacion.jsx
+++ src/components/containers/Presentacion.jsx
@@ -3,19 +3,37 @@ import RedSocial from "../pure/RedSocial";
 import logod from "../../imagenes/presentacion/icon 2.svg";
 import RedesSociales from "./RedesSociales";
 import { AiOutlineCloudDownload } from "react-icons/ai";
+import foto from '../../imagenes/presentacion/foto sin fondo.png'
+
+import "../../styles/presentacion.css";
 
 const Presentacion = () => {
   return (
-    <div>
-      <h2>
-        Hola, <br /> mi nombre es Jeasson <br /> Ingeniero de Sistemas,
-        Desarrollador Frontend
-      </h2>
-      <button className="btn">
-        Descargar CV
-        <AiOutlineCloudDownload className="ri"/>
-      </button>
-      <RedesSociales />
+    <div className="div-contenedor-presentacion">
+      <div className="presentacion-div-text">
+        <div className="text-div-fondo">DEV</div>
+        <div className="text-div-texto">
+          <h2>
+            <span className="presentacion-span-texto-mediano">Hola, </span>{" "}
+            <br />{" "}
+            <span className="presentacion-span-texto-grande">
+              mi nombre es <br /> Jeasson
+            </span>{" "}
+            <br />{" "}
+            <span className="presentacion-span-texto-mediano">
+              Ingeniero de Sistemas, Desarrollador Frontend
+            </span>
+          </h2>
+          <button className="btn">
+            Descargar CV
+            <AiOutlineCloudDownload className="ri" />
+          </button>
+          <RedesSociales />
+        </div>
+      </div>
+      <div className="presentacion-div-img">
+        <img src={foto} alt="foto" title="foto" className="img-foto"/>
+      </div>
     </div>
   );
 };
